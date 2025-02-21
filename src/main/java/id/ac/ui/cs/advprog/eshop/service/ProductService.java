7345bf5cb32b2ed06eee6bc95e3b6eb8e3e7908c
agustinisridewi <agus.tini31@ui.ac.id>
2025-02-21 15:14:47 +0700

    fix more warning attempt

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,11 +4,11 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
+    Product create(Product product);
+    List<Product> findAll();
 
-    public Product edit(String id, Product product);
-    public Product findProductById(String id);
-    public void delete(Product product);
+    Product edit(String id, Product product);
+    Product findProductById(String id);
+    void delete(Product product);
 
 }
